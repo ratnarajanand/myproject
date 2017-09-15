671553d83188c88c048ae9436201523c79fc6635
ratnarajanand <ratnarajanand.gmail.com>
2017-09-15 15:30:45 +0530

    second Commit

--- core/src/main/java/com/myproj/myproject/core/filters/LoggingFilter.java
+++ core/src/main/java/com/myproj/myproject/core/filters/LoggingFilter.java
@@ -13,7 +13,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.myproj.myproject.core.filters;
+package com.myproj.myproject.core.filters; 
+
 
 import java.io.IOException;
 import javax.servlet.Filter;
